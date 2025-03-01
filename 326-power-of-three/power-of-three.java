3d2790fdee8758d138bc69cced53caf4dba05377
Shubham Kumar Gond <161489941+aadvikshubh11@users.noreply.github.com>
2025-03-01 16:59:50 +0530

    Time: 8 ms (90.46%) | Memory: 44.3 MB (26.58%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        // If n is less than or equal to 0, it's not a power of 3
-        if (n <= 0) return false;
-        
-        // Repeatedly divide n by 3
-        while (n % 3 == 0) {
-            n /= 3;
+        if(n<1){
+            return false;
+        }else if(n==1){
+            return true;
         }
-        
-        // If n is reduced to 1, it is a power of 3
-        return n == 1;
+        else {
+            while(n%3==0){
+                n=n/3;
+            }
+            if(n==1) return true;
+            else return false;
+        }
     }
 }
